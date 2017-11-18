483646b8afc0a5ed9ccc96bd4bdef27a12afe640
Anna Zacharewicz <anna.zacharewicz@yahoo.com>
2017-11-18 23:00:39 +0100

    Zadanie domowe 18.2

--- zajecia18_zadanieDomowe_Maven_zadanie2/src/main/java/pl/javastart/couponscalc/PriceCalculator.java
+++ zajecia18_zadanieDomowe_Maven_zadanie2/src/main/java/pl/javastart/couponscalc/PriceCalculator.java
@@ -6,15 +6,15 @@ public class PriceCalculator {
 
     List<Product> products = null;
     List<Coupon> coupons = null;
-    double totalPrice = 0.00;
-    double priceWithDiscount = 0.00;
+    double totalPrice = 0.;
 
     public double calculatePrice(List<Product> products, List<Coupon> coupons) {
-        for (Product product: products) {
-            if(coupons == null) {
-                totalPrice = totalPrice + product.getPrice();
-            }
-        }
+
+//        for (Product product: products) {
+//            if(coupons == null) {
+//                totalPrice = totalPrice + product.getPrice();
+//            }
+//        }
         return totalPrice;
     }
 
