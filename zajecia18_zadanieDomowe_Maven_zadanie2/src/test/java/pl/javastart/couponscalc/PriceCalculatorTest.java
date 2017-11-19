829ba74947dd299cd0dde7e9772c2509458cfb4c
Anna Zacharewicz <anna.zacharewicz@yahoo.com>
2017-11-19 21:22:07 +0100

    Zadanie 18.2

--- zajecia18_zadanieDomowe_Maven_zadanie2/src/test/java/pl/javastart/couponscalc/PriceCalculatorTest.java
+++ zajecia18_zadanieDomowe_Maven_zadanie2/src/test/java/pl/javastart/couponscalc/PriceCalculatorTest.java
@@ -1,12 +1,10 @@
 package pl.javastart.couponscalc;
 
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+        import org.junit.Test;
+        import java.util.ArrayList;
+        import java.util.List;
+        import static org.hamcrest.CoreMatchers.is;
+        import static org.junit.Assert.*;
 
 public class PriceCalculatorTest {
 
@@ -19,7 +17,7 @@ public class PriceCalculatorTest {
         double result = priceCalculator.calculatePrice(null, null);
 
         // then
-        assertThat(result, is(0.00));
+        assertThat(result, is(0.));
     }
 
     @Test
@@ -55,62 +53,45 @@ public class PriceCalculatorTest {
         assertThat(result, is(4.79));
     }
 
-//    Więcej niż jeden kod rabatowy:
-//        • kody rabatowe nie łączą się, a w przypadku zastosowania więcej niż jednego
-//        sklep idzie zawsze na rękę klientowi i wybiera najkorzystniejszy rabat.
-
-//        przykład 1:
-//        masło za 6zł i opony za 100zł, oraz kody rabatowe na jedzenie o wartości 50% i na art. samochodowe 10%
-//        - wybrany zostanie kupon na art samochodowe ponieważ obniżka ot 10zł, a spożywczy dałby zniżkę 3zł.
     @Test
-    public void shouldReturnPriceForTwoProductsAndCouponsOnCategory() {
+    public void shouldReturnPriceForSingleProductAndOneCouponWithNoCategory() {
 
         // given
         PriceCalculator priceCalculator = new PriceCalculator();
         List<Product> products = new ArrayList<>();
-        products.add(new Product("Masło", 6.00, Category.FOOD));
-        products.add(new Product("Opony", 100.00, Category.CAR));
+        products.add(new Product("Masło", 5.99, Category.FOOD));
 
         List<Coupon> coupons = new ArrayList<>();
-        coupons.add(new Coupon(Category.FOOD, 50));
-        coupons.add(new Coupon(Category.CAR, 10));
+        coupons.add(new Coupon(null, 20));
 
         // when
         double result = priceCalculator.calculatePrice(products, coupons);
 
         // then
-        assertThat(result, is(96.00));
+        assertThat(result, is(4.79));
     }
 
-
-//        przykład 2:
-//        rabat na wszystko w wysokości 10% oraz rabat na jedzenie w wysokości 50%
-//        - klient kupuje tylko artykuły spożywcze
-//        - powinien zostać zastosowany rabat na jedzenie
     @Test
-    public void shouldReturnPriceForTwoProductsAndCouponOnCategoryAndCouponOnAllProducts() {
+    public void shouldReturnPriceMoreProductsAndOneCoupon() {
 
         // given
         PriceCalculator priceCalculator = new PriceCalculator();
         List<Product> products = new ArrayList<>();
-        products.add(new Product("Cukierki", 7.00, Category.FOOD));
-        products.add(new Product("Mleko", 3.00, Category.FOOD));
+        products.add(new Product("Masło", 5.99, Category.FOOD));
+        products.add(new Product("Bułka", 1.99, Category.FOOD));
+        products.add(new Product("Żarówka", 1.99, Category.HOME));
 
         List<Coupon> coupons = new ArrayList<>();
-        coupons.add(new Coupon(Category.FOOD, 50));
-        coupons.add(new Coupon(null, 10));
+        coupons.add(new Coupon(Category.FOOD, 20));
 
         // when
         double result = priceCalculator.calculatePrice(products, coupons);
 
         // then
-        assertThat(result, is(5.00));
+        assertThat(result, is(8.37));
     }
 
 }
-
-
-
 
 
 
